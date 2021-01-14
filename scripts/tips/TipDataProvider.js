eea8c3ba441c4b6e77edd428efd896d0423ff4df
daprieu <daprieu@gmail.com>
2021-01-14 09:14:04 -0600

    edited Tips section to look better, and add new subdir for travel and corresponding files via terminal

--- scripts/tips/TipDataProvider.js
+++ scripts/tips/TipDataProvider.js
@@ -1,10 +1,10 @@
 const tips = [
     {
         filtration: "Check Filtration, pumps running and filters are clean",
-        ph: "pH can be fish specific and needs to be checked regularly",
+        pH: "pH can be fish specific and needs to be checked regularly",
         nitrates: "Nitrates can be toxic to fish and need to be eliminated",
-        food: "feeding too much or too often can created more fish waste inreacsing nitrates or cause a bacteria bloom both are negatives and you want to avoid",
-        water: "sometimes to get things back to normal is doing regular water changes roughly 20% weekly",
+        food: "Feeding too much or too often can created more fish waste inreacsing nitrates or cause a bacteria bloom both are negatives and you want to avoid",
+        water: "Sometimes to get things back to normal is doing regular water changes roughly 20% weekly",
        
     },
 
